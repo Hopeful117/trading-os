f284d9e94dc9d90ef02bf87e1b904bf3c5361cdf
ludo <ludovic.brot@gmail.com>
2026-07-04 13:45:09 +0200

    init core services

---
+++ trading-core/src/main/java/com/hope/trading/trading_core/TradingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hope.trading.trading_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingCoreApplication.class, args);
+	}
+
+}
---
+++ trading-core/src/test/java/com/hope/trading/trading_core/TradingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hope.trading.trading_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
