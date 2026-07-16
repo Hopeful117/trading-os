e83f4eeb057a723576adb51652e29a21bffc86b4
ludo <ludovic.brot@gmail.com>
2026-07-16 16:50:06 +0200

    approche observable frontend

--- trading-os-web/src/app/features/accounts/pages/accounts/accounts.ts
+++ trading-os-web/src/app/features/accounts/pages/accounts/accounts.ts
@@ -2,52 +2,32 @@ import { Component } from '@angular/core';
 import { AccountService } from '../../../../core/services/account.service';
 import { Account } from '../../../../core/models/account.model';
 import { AccountCard} from '../../components/account-card/account-card';
-import { ChangeDetectorRef} from '@angular/core';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-accounts',
-  imports: [AccountCard],
+  imports: [AccountCard, AsyncPipe],
   templateUrl: './accounts.html',
   styleUrl: './accounts.scss',
 })
 export class Accounts {
-  accounts: Account[] = [];
+  accounts!: Observable<Account[]>;
   loadingSync = false;
   errorMessage = '';
 
-  constructor(
-    private accountService: AccountService,
-    private cdr: ChangeDetectorRef
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.loadAccounts();
   }
 
   private loadAccounts(): void {
-    this.accountService.getAccounts().subscribe({
-      next: (accounts) => {
-        this.accounts = accounts;
-        this.cdr.detectChanges();
-      },
-      error: (error) => {
-        this.loadingSync = false;
-
-        if (error.status === 401) {
-          this.errorMessage = 'Votre session a expiré, veuillez vous reconnecter.';
-        } else if (error.status === 403) {
-          this.errorMessage = 'Vous n’avez pas les droits pour accéder à ces comptes.';
-        } else if (error.status === 404) {
-          this.errorMessage = 'Aucun compte trouvé.';
-        } else if (error.status === 503) {
-          this.errorMessage = 'Le service de synchronisation est indisponible.';
-        } else {
-          this.errorMessage = 'Une erreur est survenue lors du chargement des comptes.';
-        }
-      },
-    });
+    this.accounts = this.accountService.getAccounts();
   }
+
   sync(): void {
     this.loadingSync = true;
 
@@ -56,9 +36,9 @@ export class Accounts {
         this.loadingSync = false;
         this.loadAccounts();
       },
-
       error: () => {
         this.loadingSync = false;
+        this.errorMessage = 'Erreur lors de la synchronisation.';
       },
     });
   }
