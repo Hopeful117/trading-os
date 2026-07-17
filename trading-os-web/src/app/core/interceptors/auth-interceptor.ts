c6b4059e2ccc40b4faed8979bb17e501d78651f4
ludo <ludovic.brot@gmail.com>
2026-07-17 17:22:42 +0200

    update auth interceptor

--- trading-os-web/src/app/core/interceptors/auth-interceptor.ts
+++ trading-os-web/src/app/core/interceptors/auth-interceptor.ts
@@ -1,26 +1,41 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { TokenService } from '../services/token';
+import {AuthService} from '../services/auth.service';
 import { inject } from '@angular/core';
+import { provideRouter, Router} from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
 
-  if (req.url.includes('/login')) {
+  if (req.url.includes('/login') || req.url.includes('/register')) {
     return next(req);
   }
 
   const token = tokenService.getToken();
 
-  if (!token) {
-    return next(req);
+  let request = req;
+
+  if (token) {
+    request = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 
-  const authenticatedRequest = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  return next(request).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        authService.logout();
 
-  return next(authenticatedRequest);
+        router.navigate(['/']);
+      }
+
+      return throwError(() => error);
+    }),
+  );
 };
